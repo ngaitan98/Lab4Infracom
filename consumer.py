342073d4c33e58fe9dbcca933c9b7140809b4b66
ngaitan98 <n.gaitan@uniandes.edu.co>
2019-03-23 19:32:22 -0500

    Hi

--- consumer.py
+++ consumer.py
@@ -14,12 +14,12 @@ def video():
 	print("Nuevo cliente: " + topic)
 	os.system("producer.py " + topic)
 	consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    return Response(get_video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+	return Response(get_video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def get_video_stream():
-    for msg in consumer:
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n')
+	for msg in consumer:
+		yield (b'--frame\r\n'
+			b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0', debug=True)
