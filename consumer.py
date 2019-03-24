eb9ef7dc86733841684c61d697ec0c64df279c0f
ngaitan98 <n.gaitan@uniandes.edu.co>
2019-03-23 19:48:29 -0500

    Hi

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ import datetime
 import os
 from flask import Flask, Response, request
 from kafka import KafkaConsumer
-
+from threading import Thread
 
 
 # Set the consumer in a Flask App
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def video():
 	topic = str(request.remote_addr)
 	print("Nuevo cliente: " + topic)
-	os.system("python producer.py " + topic)
+	Thread(target = executeProducer, args = [topic]).start()
 	print('Disfrute pez')
 	consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
 	return Response(get_video_stream(consumer), mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -21,6 +21,8 @@ def get_video_stream(consumer):
 	for msg in consumer:
 		yield (b'--frame\r\n'
 			b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n')
+def executeProducer(topic):
+	os.system("python producer.py " + topic)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
